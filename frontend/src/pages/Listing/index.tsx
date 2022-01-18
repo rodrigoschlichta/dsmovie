315dc3b521a6f3e3aed0707506d2d5113e9c81d8
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2022-01-18 09:57:52 -0300

    new deploy

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import MovieCard from '../../components/MovieCard';
 import Pagination from '../../components/Pagination';
 import { MoviePage } from '../../types/movie';
